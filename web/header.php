5cf6d816568b0f62c59dfeaef15d663bccf308c8
Kijam Lopez <kijamve@gmail.com>
2015-04-02 01:35:18 -0430

    Se añaden nuevos keywords

--- web/header.php
+++ web/header.php
@@ -4,7 +4,7 @@
 	<title><?php echo $html_title; ?> - Consultas de Cédulas Venezolanas</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="keywords" content="api,cedula,cedulas,venezuela,venezolanas,venezolanos,consultas,webservice,json" />
+	<meta name="keywords" content="api,cedula,saime,onidex,cne,venezuela,venezolanas,venezolanos,consultas,webservice,json,xml" />
 	<meta name="description" content="<?php echo $html_description; ?>" />
 	<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
 	<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
