152632e8d22930ebb7602638026a83657b5fa08a
Kijam Lopez <kijamve@gmail.com>
2015-04-01 20:39:56 -0430

    Códigos de ejemplo para consultas al api

---
+++ client_example/example.js
@@ -0,0 +1,31 @@
+var request = require("request")
+var APPID_CEDULA = 'APP-ID-AQUI';
+var TOKEN_CEDULA = 'TOKEN-AQUI';
+
+function getCI(cedula, cb) {
+	request({
+		url: 'https://cuado.co:444/api/v1?app_id='+APPID_CEDULA+'&token='+TOKEN_CEDULA+'&cedula='+cedula,
+		json: true,
+		rejectUnauthorized: false
+	}, function (error, response, body) {
+		if (!error && response.statusCode === 200) {
+			if(body.data)
+				return cb(true, body.data, false);
+			else 
+				return cb(true, false, body.error_str);
+		}else{
+			cb(false, false, false);
+		}
+	});
+}
+
+
+getCI('00000', function(result, data, error_str){
+	if(result)
+		if(error_str)
+			console.log('Ocurrio un error: '+error_str);
+		else 
+			console.log(data);
+	else
+		console.log('Ocurrio un error en la consulta');
+});
---
+++ client_example/example.py
@@ -0,0 +1,23 @@
+import urllib.request
+import json
+
+APPID_CEDULA = 'APP-ID-AQUI'
+TOKEN_CEDULA = 'TOKEN-AQUI'
+
+def getCI( cedula ):
+	global APPID_CEDULA
+	global TOKEN_CEDULA
+	response = urllib.request.urlopen('https://cuado.co:444/api/v1?app_id='+APPID_CEDULA+'&token='+TOKEN_CEDULA+'&cedula='+cedula).read()
+	if response:
+		data = json.loads(response.decode("utf-8"))
+		if data['data']:
+			return data['data']
+	return false
+	
+persona = getCI('00000')
+if persona:
+	print(persona['primer_nombre'])
+	print(persona['primer_apellido'])
+	print(persona)
+else:
+	print('Ocurrio un error')
