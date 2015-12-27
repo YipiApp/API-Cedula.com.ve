4df008914c2fff5b15c22bfca6066cef924ca180
Desarrollos Cuado C.A <info@cuado.co>
2015-12-27 01:24:23 -0430

    Se elimino duplicado de google analytics

--- web/test.php
+++ web/test.php
@@ -133,15 +133,5 @@
     </script>
 </div>
 </form>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', '<?php echo GOOGLE_ANALYTICS_KEY; ?>', 'auto');
-  ga('send', 'pageview');
-
-</script>
 <?php
     include('footer.php');
